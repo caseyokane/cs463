b1510c5c59f84ddd270c1af7289788c24f1e2427
caseyokane <casey.okane@uky.edu>
2016-08-31 09:44:36 -0400

    Starting implementation of alternative graphics

--- Prog1/puzzle1.py
+++ Prog1/puzzle1.py
@@ -6,6 +6,8 @@ from graphics import *
 
 def drawChaos(window):
     #current plan, draw a large rectangle and redraw tubes as click occurs
+
+"""
     lCorner = Point(50,50)
     rCorner = Point(350,350)
     tubeContainer = Rectangle(lCorner, rCorner)
@@ -26,6 +28,18 @@ def drawChaos(window):
     t3Line.draw(window)
     t2Line.draw(window)
     t1Line.draw(window)
+"""
+
+    #The below idea is to draw individual tubes and then move them around
+    #Maybe look into QGraphicsItemGroup
+
+    #Still need to account for making some rectangles smaller, change up corner
+    leftT6upCorner = Point(50,50); leftT6lowCorner = Point(200,100);
+    leftT5upCorner = Point(50,100); leftT5lowCorner = Point(200,100);
+    leftT4upCorner = Point(50,150); leftT4lowCorner = Point(200,150);
+    leftT3upCorner = Point(50,200); leftT3lowCorner = Point(200,200);
+    leftT2upCorner = Point(50,250); leftT2lowCorner = Point(200,250);
+    leftT1upCorner = Point(50,300); leftT1lowCorner = Point(200,350);     
 
 def main():
     #Currently just a dummy program that creates a small window with a circle
